bb08beed435e3d46aea130722de96b9361bf4fd2
AlxTray <alex.ashby02@icloud.com>
2024-08-13 15:19:23 +0100

    refactor: removed needless shadow colour in hyprland config

--- homeManagerModules/hypr/hyprland.nix
+++ homeManagerModules/hypr/hyprland.nix
@@ -94,13 +94,10 @@ in
                         drop_shadow = true
                         shadow_range = 4
                         shadow_render_power = 3
-                        col.shadow = rgba(1a1a1aee)
                         blur {
                           enabled = true
                           size = 5
                           passes = 3
-                          new_optimizations = on
-                          ignore_opacity = off
                         }
                       }
             
