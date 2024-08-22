bcddfff9b70ac98109cd221530558b7c243a1d5e
AlxTray <alex.ashby02@icloud.com>
2024-08-22 22:28:59 +0100

    feat: back to unstable with lock update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ZaneyOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
