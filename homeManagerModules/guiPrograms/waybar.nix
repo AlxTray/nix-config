658098e33dc10ea7d7237385a8d5383d4bb81ab1
AlxTray <alex.ashby02@icloud.com>
2024-08-23 20:17:38 +0100

    feat: waybar changes

--- homeManagerModules/guiPrograms/waybar.nix
+++ homeManagerModules/guiPrograms/waybar.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:
+{ lib, config, ... }:
 
 {
   options = {
@@ -11,172 +11,183 @@
     programs.waybar = {
       enable = true;
       settings = [
-  	    {
-      	  margin-top = 0;
-      	  margin-left = 120;
-      	  margin-bottom = 0;
-      	  margin-right = 120;
-      	  height = 60; 
-      	  layer = "top";
-      	  position = "top";
-      	  spacing = 15;
-      	  modules-left = ["custom/launcher" "clock" "clock#date"];
-      	  modules-center = ["hyprland/workspaces"];
-      	  modules-right = ["pulseaudio" "memory" "tray" "custom/powermenu"];
+        {
+          margin-top = 0;
+          margin-left = 120;
+          margin-bottom = 0;
+          margin-right = 120;
+          height = 52;
+          layer = "top";
+          position = "top";
+          spacing = 15;
+          modules-left = [ "custom/launcher" "clock" "clock#date" ];
+          modules-center = [ "hyprland/workspaces" ];
+          modules-right = [ "pulseaudio" "cpu" "memory" "tray" "custom/powermenu" ];
 
-      	  "hyprland/workspaces" = {
-      	      disable-scroll = true;
-      	      all-outputs = true;
-      	      on-click = "activate";
-      	      on-scroll-up = "hyprctl dispatch workspace e+1";
-      	      on-scroll-down = "hyprctl dispatch workspace e-1";
-      	  };
-	  
-      	  "custom/launcher" = {
-      	      interval = "once";
-      	      format = "<span font='FontAwesome' size='20000'> </span>";
-      	      on-click = "walker";
-      	      tooltip = false;
-      	  };
+          "hyprland/workspaces" = {
+            disable-scroll = true;
+            all-outputs = true;
+            on-click = "activate";
+            on-scroll-up = "hyprctl dispatch workspace e+1";
+            on-scroll-down = "hyprctl dispatch workspace e-1";
+          };
 
-      	  memory = {
-      	    interval = 30;
-      	    format = "  {}%";
-      	    format-alt = "  {used:0.1f}G";
-      	    max-length = 10;
-      	  };
+          "custom/launcher" = {
+            interval = "once";
+            format = "<span font='FontAwesome' size='18000'> </span>";
+            on-click = "walker";
+            tooltip = false;
+          };
 
-      	  pulseaudio = {
-      	      format = "{icon} {volume}%";
-      	      format-bluetooth = "{icon}  {volume}%";
-      	      format-bluetooth-muted = "  muted";
-      	      format-muted = " muted";
-      	      format-icons = {
-      		  headphone = " ";
-      		  hands-free = " ";
-      		  headset = " ";
-      		  phone = " ";
-      		  portable = " ";
-      		  car = " ";
-      		  default = ["" "" ""];
-      	      };
-      	      on-click = "pavucontrol";
-      	  };
+          memory = {
+            interval = 30;
+            format = "  {}%";
+            format-alt = "  {used:0.1f}G";
+            max-length = 10;
+            on-click = "kitty -e btop";
+          };
 
-      	  clock = {
-      	    format = " {:%H:%M}";
-      	  };
+          cpu = {
+            interval = 5;
+            format = "  {usage}%";
+            max-length = 10;
+            on-click = "kitty -e btop";
+          };
 
-      	  "clock#date" = {
-      	    format = " {:%A, %B %d, %Y}";
-      	  };
+          pulseaudio = {
+            format = "{icon} {volume}%";
+            format-bluetooth = "{icon}  {volume}%";
+            format-bluetooth-muted = "  muted";
+            format-muted = " muted";
+            format-icons = {
+              headphone = " ";
+              hands-free = " ";
+              headset = " ";
+              phone = " ";
+              portable = " ";
+              car = " ";
+              default = [ "" "" "" ];
+            };
+            on-click = "pavucontrol";
+          };
 
-      	  "custom/powermenu" = {
-      	    format = "<span font='FontAwesome' size='20000'> </span>";
-      	    on-click = "wlogout";
-      	    tooltip = false;
-      	  };
+          clock = {
+            format = " {:%H:%M}";
+            tooltip = false;
+          };
 
-      	  tray = {
-      	    icon-size = 21;
-      	    spacing = 3;
-      	  };  
-  	    }
+          "clock#date" = {
+            format = " {:%A, %B %d, %Y}";
+            tooltip = false;
+          };
+
+          "custom/powermenu" = {
+            format = "<span font='FontAwesome' size='18000'> </span>";
+            on-click = "wlogout";
+            tooltip = false;
+          };
+
+          tray = {
+            icon-size = 21;
+            spacing = 3;
+          };
+        }
       ];
       style = lib.concatStrings [
         ''
-          *{
-        	  font-family: Inter, FontAwesome;
-        	  font-size: 16px;
-        	  font-weight: bold;
-        	  margin: 0;
-        	  padding: 0;
-        	}
+                    *{
+                  	  font-family: Inter, FontAwesome;
+                  	  font-size: 16px;
+                  	  font-weight: bold;
+                  	  margin: 0;
+                  	  padding: 0;
+                  	}
 
-        	window#waybar {
-        	  background-color: #${config.stylix.base16Scheme.base00};
-        	  color: #${config.stylix.base16Scheme.base07};
-        	  transition-property: background-color;
-        	  transition-duration: 0.5s;
-        	  border-radius: 0px 0px 15px 15px;
-        	  transition-duration: .5s;
+                  	window#waybar {
+                  	  background-color: #${config.stylix.base16Scheme.base00};
+                  	  color: #${config.stylix.base16Scheme.base07};
+                  	  transition-property: background-color;
+                  	  transition-duration: 0.5s;
+                  	  border-radius: 0px 0px 15px 15px;
+                  	  transition-duration: .5s;
 
-        	  border-bottom-width: 5px;
-        	  border-bottom-color: #131617;
-        	  border-bottom-style: solid;
-        	}
+                  	  border-bottom-width: 5px;
+                  	  border-bottom-color: #131617;
+                  	  border-bottom-style: solid;
+                  	}
 
-        	#custom-launcher,
-        	#clock,
-        	#clock-date,
-        	#workspaces,
-        	#pulseaudio,
-        	#memory,
-        	#custom-powermenu,
-        	#tray {
-        	  background-color: #${config.stylix.base16Scheme.base02};
-        	  color: #${config.stylix.base16Scheme.base07};
+                  	#custom-launcher,
+                  	#clock,
+                  	#clock-date,
+                  	#workspaces,
+                  	#pulseaudio,
+                    #cpu,
+                  	#memory,
+                  	#custom-powermenu,
+                  	#tray {
+                  	  background-color: #${config.stylix.base16Scheme.base02};
+                  	  color: #${config.stylix.base16Scheme.base07};
 
-        	  padding-left: 10px;
-        	  padding-right: 10px;
-        	  margin-top: 7px;
-        	  margin-bottom: 12px;
-        	  border-radius: 10px;
+                  	  padding-left: 10px;
+                  	  padding-right: 10px;
+                  	  margin-top: 7px;
+                  	  margin-bottom: 12px;
+                  	  border-radius: 10px;
 
-        	  border-bottom-width: 5px;
-        	  border-bottom-color: #463b37;
-        	  border-bottom-style: solid;
-        	}
+                  	  border-bottom-width: 5px;
+                  	  border-bottom-color: #463b37;
+                  	  border-bottom-style: solid;
+                  	}
 
-        	#workspaces {
-        	  padding: 0;
-        	}
+                  	#workspaces {
+                  	  padding: 0;
+                  	}
 
-        	#workspaces button {
-        	  padding-left: 15px;
-        	  padding-right: 15px;
-        	}
+                  	#workspaces button {
+                  	  padding-left: 15px;
+                  	  padding-right: 15px;
+                  	}
 
-        	#workspaces button:hover { 
-            background: #${config.stylix.base16Scheme.base02};
-            border: #463b37;
-        	  border-radius: 10px;
-          }
+                  	#workspaces button:hover { 
+                      background: #${config.stylix.base16Scheme.base02};
+                      border: #463b37;
+                  	  border-radius: 10px;
+                    }
 
-        	#workspaces button.active {
-        	  background-color: #${config.stylix.base16Scheme.base0D};
-        	  color: #${config.stylix.base16Scheme.base00};
+                  	#workspaces button.active {
+                  	  background-color: #${config.stylix.base16Scheme.base0D};
+                  	  color: #${config.stylix.base16Scheme.base00};
 
-        	  border-radius: 10px;
-	  
-        	  padding-left: 20px;
-        	  padding-right: 20px;
-        	  margin-bottom: -5px;
+                  	  border-radius: 10px;
+          	  
+                  	  padding-left: 20px;
+                  	  padding-right: 20px;
+                  	  margin-bottom: -5px;
 
-        	  border-bottom-width: 5px;
-        	  border-bottom-color: #637f78;
-        	  border-bottom-style: solid;
-        	}
+                  	  border-bottom-width: 5px;
+                  	  border-bottom-color: #637f78;
+                  	  border-bottom-style: solid;
+                  	}
 
-        	#custom-launcher {
-        	  background-color: #${config.stylix.base16Scheme.base0B};
-        	  color: #${config.stylix.base16Scheme.base00};
-        	  border-bottom-color: #989b06;
+                  	#custom-launcher {
+                  	  background-color: #${config.stylix.base16Scheme.base0B};
+                  	  color: #${config.stylix.base16Scheme.base00};
+                  	  border-bottom-color: #989b06;
 
-        	  margin-left: 15px;
-        	  padding-left: 14px;
-            padding-right: 9px;
-        	}
+                  	  margin-left: 15px;
+                  	  padding-left: 14px;
+                      padding-right: 9px;
+                  	}
 
-        	#custom-powermenu {
-        	  background-color: #${config.stylix.base16Scheme.base08};
-        	  color: #${config.stylix.base16Scheme.base00};
-        	  border-bottom-color: #db2914;
+                  	#custom-powermenu {
+                  	  background-color: #${config.stylix.base16Scheme.base08};
+                  	  color: #${config.stylix.base16Scheme.base00};
+                  	  border-bottom-color: #db2914;
 
-        	  margin-right: 15px;
-        	  padding-left: 14px;
-            padding-right: 7px;
-        	}
+                  	  margin-right: 15px;
+                  	  padding-left: 14px;
+                      padding-right: 7px;
+                  	}
         ''
       ];
     };
