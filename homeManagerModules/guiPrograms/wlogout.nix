0246a320399f1e62e637edb7456a2516c1778879
AlxTray <alex.ashby02@icloud.com>
2024-09-01 23:30:41 +0100

    wlogout: updated theming

--- homeManagerModules/guiPrograms/wlogout.nix
+++ homeManagerModules/guiPrograms/wlogout.nix
@@ -12,50 +12,52 @@
       layout = [
         {
           label = "shutdown";
-          action = "sleep 1; systemctl poweroff";
+          action = "systemctl poweroff";
           text = "Shutdown";
           keybind = "s";
         }
         {
-          "label" = "reboot";
-          "action" = "sleep 1; systemctl reboot";
-          "text" = "Reboot";
-          "keybind" = "r";
+          label = "reboot";
+          action = "systemctl reboot";
+          text = "Reboot";
+          keybind = "r";
         }
         {
-          "label" = "logout";
-          "action" = "sleep 1; hyprctl dispatch exit";
-          "text" = "Exit";
-          "keybind" = "e";
+          label = "logout";
+          action = "hyprctl dispatch exit";
+          text = "Exit";
+          keybind = "e";
         }
         {
-          "label" = "suspend";
-          "action" = "sleep 1; systemctl suspend";
-          "text" = "Suspend";
-          "keybind" = "u";
+          label = "suspend";
+          action = "systemctl suspend";
+          text = "Suspend";
+          keybind = "u";
         }
         {
-          "label" = "lock";
-          "action" = "sleep 1; hyprlock";
-          "text" = "Lock";
-          "keybind" = "l";
+          label = "lock";
+          action = "hyprlock";
+          text = "Lock";
+          keybind = "l";
         }
         {
-          "label" = "hibernate";
-          "action" = "sleep 1; systemctl hibernate";
-          "text" = "Hibernate";
-          "keybind" = "h";
+          label = "hibernate";
+          action = "systemctl hibernate";
+          text = "Hibernate";
+          keybind = "h";
         }
       ];
       style = ''
         * {
-          font-family: Inter, FontAwesome, sans-serif;
+          font-family: ${config.stylix.fonts.sansSerif.name}, FontAwesome;
         	background-image: none;
         	transition: 20ms;
         }
+
         window {
-        	background-color: rgba(12, 12, 12, 0.1);
+        	background-color: rgba(12, 12, 12, 0.6);
         }
+
         button {
         	color: #${config.stylix.base16Scheme.base05};
           font-size:20px;
@@ -63,42 +65,48 @@
         	background-position: center;
         	background-size: 25%;
         	border-style: solid;
+          border-radius: 10px;
         	background-color: rgba(12, 12, 12, 0.3);
-        	border: 3px solid #${config.stylix.base16Scheme.base05};
-          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        	border: 3px solid #${config.stylix.base16Scheme.base03};
         }
+
         button:focus,
         button:active,
         button:hover {
-          color: #${config.stylix.base16Scheme.base0B};
           background-color: rgba(12, 12, 12, 0.5);
-          border: 3px solid #${config.stylix.base16Scheme.base0B};
+          border: 3px solid #${config.stylix.base16Scheme.base0D};
         }
+
         #logout {
         	margin: 10px;
         	border-radius: 20px;
         	background-image: image(url("icons/logout.png"));
         }
+
         #suspend {
         	margin: 10px;
         	border-radius: 20px;
         	background-image: image(url("icons/suspend.png"));
         }
+
         #shutdown {
         	margin: 10px;
         	border-radius: 20px;
         	background-image: image(url("icons/shutdown.png"));
         }
+
         #reboot {
         	margin: 10px;
         	border-radius: 20px;
         	background-image: image(url("icons/reboot.png"));
         }
+
         #lock {
         	margin: 10px;
         	border-radius: 20px;
         	background-image: image(url("icons/lock.png"));
         }
+
         #hibernate {
         	margin: 10px;
         	border-radius: 20px;
