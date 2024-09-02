8aeb5a8a119d2d161cbde5bedf27073cf2e0e0b4
AlxTray <alex.ashby02@icloud.com>
2024-09-02 13:38:16 +0100

    boot: made it as silent as possible

--- nixosModules/boot/plymouth.nix
+++ nixosModules/boot/plymouth.nix
@@ -7,6 +7,16 @@
   };
 
   config = lib.mkIf config.plymouthModule.enable {
-    boot.plymouth.enable = true;
+    boot = {
+      consoleLogLevel = 3;
+      kernelParams = [
+        "quiet"
+        "splash"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+      ];
+      plymouth.enable = true;
+    };
   };
 }
