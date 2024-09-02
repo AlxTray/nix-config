0dfd274b7c893be520619c43099d9c7dcd5dd6ba
AlxTray <alex.ashby02@icloud.com>
2024-09-02 09:28:13 +0100

    git: added delta

--- homeManagerModules/terminalPrograms/git.nix
+++ homeManagerModules/terminalPrograms/git.nix
@@ -7,8 +7,17 @@
   };
 
   config = lib.mkIf config.gitModule.enable {
+    home.sessionVariables.DELTA_PAGER = "less -R";
     programs.git = {
       enable = true;
+      delta = {
+        enable = true;
+        options.dark = true;
+      };
+      extraConfig = {
+        diff.colorMoved = "default";
+        merge.conflictstyle = "diff3";
+      };
       userName = "AlxTray";
       userEmail = "alex.ashby02@icloud.com";
     };
