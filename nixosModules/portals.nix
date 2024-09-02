51c8056f56700a6747606d4ea8674854cb74a6d2
AlxTray <alex.ashby02@icloud.com>
2024-09-02 08:47:29 +0100

    portals: made xdg open use portals

--- nixosModules/portals.nix
+++ nixosModules/portals.nix
@@ -9,6 +9,11 @@
   config = lib.mkIf config.portalsModule.enable {
     xdg.portal = {
       enable = true;
+      xdgOpenUsePortal = true;
+      config = {
+        common.default = [ "gtk" ];
+        hyprland.default = [ "gtk" "hyprland" ];
+      };
       extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     };
   };
